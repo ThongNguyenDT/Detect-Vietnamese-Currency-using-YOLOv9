5f875c4354dd38d6a1b9a96334a2afaa90bd2d10
Thong Nguyen <nguyenduongtienthong@gmail.com>
2024-03-13 12:56:11 +0700

    second model after some adj deploy

--- deploy2roboflow_version1.py
+++ deploy2roboflow_version1.py
@@ -2,6 +2,6 @@ from roboflow import Roboflow
 
 rf = Roboflow(api_key="m8k8Zs21EaUpsthRO9Nr")
 project = rf.workspace("first-qkz37").project("vietnamese-currency-lgi9i")
-version = project.version(1)
+version = project.version(2)
 
-version.deploy(model_type="yolov9", model_path="train\\exp1")
+version.deploy(model_type="yolov9", model_path="train\\exp2")
