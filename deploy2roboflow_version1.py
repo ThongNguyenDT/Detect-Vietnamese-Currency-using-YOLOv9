fdb34e746b79f975e63844ddda920e2404978d41
Thong Nguyen <nguyenduongtienthong@gmail.com>
2024-05-12 01:07:27 +0700

    remove trash

--- deploy2roboflow_version1.py
+++
@@ -1,7 +0,0 @@
-from roboflow import Roboflow
-
-rf = Roboflow(api_key="m8k8Zs21EaUpsthRO9Nr")
-project = rf.workspace("first-qkz37").project("vietnamese-currency-lgi9i")
-version = project.version(4)
-
-version.deploy(model_type="yolov9", model_path="train\\exp2")
